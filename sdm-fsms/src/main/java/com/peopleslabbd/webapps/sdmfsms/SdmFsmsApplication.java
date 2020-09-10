ae9e73aeb474cdd67dcafd5503702e8d1d095db2
mahmudul.hasan <mahmudul.hasan@commlinkinfotech.com>
2020-09-11 04:33:20 +0600

    Project Structure added initially.

---
+++ sdm-fsms/src/main/java/com/peopleslabbd/webapps/sdmfsms/SdmFsmsApplication.java
@@ -0,0 +1,13 @@
+package com.peopleslabbd.webapps.sdmfsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmFsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmFsmsApplication.class, args);
+	}
+
+}
---
+++ sdm-fsms/src/test/java/com/peopleslabbd/webapps/sdmfsms/SdmFsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peopleslabbd.webapps.sdmfsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmFsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
